a1d971b7af85869902aa1d51d7d8279582be04b1
hbngo21 <ngothihabac2772001@gmail.com>
2022-12-07 20:33:27 +0700

    Add input information component, update input text, input password

--- frontend/src/components/form/InputInformation.js
+++ frontend/src/components/form/InputInformation.js
@@ -2,21 +2,18 @@ import * as React from "react";
 import { TextInput } from "react-native-paper";
 import { Color, TextStyle, ScreenSize } from "../../utils";
 
-export const InputText = (props) => {
-  const [text, setText] = React.useState("");
+export const InputInformation = (props) => {
+  const {title, information} = props;
   return (
     <TextInput
-      label={props.title}
-      placeholder={props.placeholder}
-      placeholderTextColor={Color.grey_100}
+      label={title}
       mode="outlined"
-      value={text}
+      value={information}
       outlineColor={Color.primary_100}
       activeOutlineColor={Color.primary_100}
-      onChangeText={(text) => setText(text)}
+      editable={false}
       style={{
         ...TextStyle.h3,
-        fontWeight: '400',
         width: (327 / 375) * ScreenSize.width,
         backgroundColor: Color.white_100,
       }}
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,6 @@
 export { AppName } from "./AppName";
 export { InputText } from "./form/InputText";
+export { InputInformation } from "./form/InputInformation";
 export { InputPassword } from "./form/InputPassword";
 export { CardService } from "./CardService";
 export { WelcomeText } from "./WelcomeText";
