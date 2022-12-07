3b3a1aafc5e33e55a1eb737f5311b06136d7bb19
hbngo21 <ngothihabac2772001@gmail.com>
2022-12-07 23:37:30 +0700

    Implement button option component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -10,3 +10,4 @@ export { ButtonMomo } from "./button/ButtonMomo";
 export { ButtonHalfWidth } from "./button/ButtonHalfWidth";
 export { ButtonSmall } from "./button/ButtonSmall";
 export { ButtonIcon } from "./button/ButtonIcon";
+export { ButtonOption } from "./button/ButtonOption";
