5de2197a16bc1b5637c8d34419eacfa3b3ac2610
hbngo21 <ngothihabac2772001@gmail.com>
2022-12-14 15:11:32 +0700

    api to change password

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -14,6 +14,7 @@ import { Response } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { CheckUserDto } from './dtos/check-user.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { ChangePasswordDto } from "./dtos/change-password.dto";
 
 @Controller('/users')
 export class UserController {
@@ -43,5 +44,12 @@ export class UserController {
   @HttpCode(HttpStatus.CREATED)
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
+  }
+
+  @Post('/:id')
+  @HttpCode(HttpStatus.CREATED)
+  async changePassword(@Body() changePasswordDto: ChangePasswordDto, @Res() res: Response) {
+    const user = await this.userService.changePassword(changePasswordDto);
+    return res.status(HttpStatus.OK).send(user);
   }
 }
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './user.schema';
-import { Model } from 'mongoose';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { CheckUserDto } from './dtos/check-user.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User, UserDocument } from "./user.schema";
+import { Model } from "mongoose";
+import { CreateUserDto } from "./dtos/create-user.dto";
+import { CheckUserDto } from "./dtos/check-user.dto";
+import { ChangePasswordDto } from "./dtos/change-password.dto";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+  }
 
   async getAllUsers(): Promise<User[]> {
     return this.userModel.find();
@@ -17,7 +19,7 @@ export class UserService {
     const user = await this.userModel.findById(id);
 
     if (!user) {
-      throw new NotFoundException('User not found!');
+      throw new NotFoundException("User not found!");
     }
 
     return user;
@@ -26,11 +28,19 @@ export class UserService {
   async getUserByRoleAndTel(checkUserDto: CheckUserDto): Promise<User> {
     return this.userModel.findOne({
       role: checkUserDto.role,
-      tel: checkUserDto.tel,
+      tel: checkUserDto.tel
     });
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     return this.userModel.create(createUserDto);
+  }
+
+  async changePassword(changePasswordDto: ChangePasswordDto): Promise<User> {
+    await this.userModel.findOneAndUpdate({
+      role: changePasswordDto.role,
+      tel: changePasswordDto.tel,
+      password: changePasswordDto.password
+    });
   }
 }
