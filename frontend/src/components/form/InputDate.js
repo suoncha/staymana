d12d4f26b470f6585c76ac09e75c5cbddb2c2386
hbngo21 <ngothihabac2772001@gmail.com>
2022-12-15 11:37:08 +0700

    Update input and modal components

--- frontend/src/components/form/InputDate.js
+++ frontend/src/components/form/InputDate.js
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { TextInput } from "react-native-paper";
-import { Text, View } from "react-native";
-import { Color, TextStyle, ScreenSize } from "../../utils";
+import React, {useState} from "react";
+import {TextInput} from "react-native-paper";
+import {Text, View} from "react-native";
+import {Color, ScreenSize, TextStyle} from "../../utils";
 import DateTimePicker from '@react-native-community/datetimepicker'
 
 export const InputDate = (props) => {
-  const { title } = props;
+    const {title} = props;
 
-  const [date, setDate] = useState(new Date())
-  const [dateShow, setDateShow] = useState(false)
-  const [dateText, setDateText] = useState(new Date())
-  const dateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date
-    setDateShow(Platform.OS == 'ios')
-    setDate(currentDate)
+    const [date, setDate] = useState(new Date())
+    const [dateShow, setDateShow] = useState(false)
+    const [dateText, setDateText] = useState(new Date())
+    const dateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date
+        setDateShow(Platform.OS == 'ios')
+        setDate(currentDate)
 
-    const tempDate = new Date(currentDate)
-    var printDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear()
-    setDateText(printDate)
-    props.output(selectedDate)
-  }
+        const tempDate = new Date(currentDate)
+        var printDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear()
+        setDateText(printDate)
+        props.output(selectedDate)
+    }
 
-  return (
-    <View>
-    <TextInput
-      label={
-        <Text style={{ ...TextStyle.h3, color: Color.dark_100 }}>{title}</Text>
-      }
-      mode="flat"
-      value={dateText}
-      activeUnderlineColor={Color.grey_100}
-      outlineColor={Color.white_100}
-      activeOutlineColor={Color.white_100}
-      editable={false}
-      style={{
-        ...TextStyle.h3,
-        fontWeight: "400",
-        width: (327 / 375) * ScreenSize.width,
-        backgroundColor: Color.white_100,
-      }}
-      right={<TextInput.Icon name='calendar-month' onPress={() => setDateShow(!dateShow)} />}
+    return (
+        <View>
+            <TextInput
+                label={
+                    <Text style={{...TextStyle.h3, color: Color.dark_100}}>{title}</Text>
+                }
+                mode="flat"
+                value={dateText}
+                activeUnderlineColor={Color.grey_100}
+                outlineColor={Color.white_100}
+                activeOutlineColor={Color.white_100}
+                editable={false}
+                style={{
+                    ...TextStyle.h3,
+                    fontWeight: "400",
+                    width: (327 / 375) * ScreenSize.width,
+                    backgroundColor: 'transparent',
+                }}
+                right={<TextInput.Icon name='calendar-month' onPress={() => setDateShow(!dateShow)}/>}
 
-    />
-    {dateShow && (
-        <DateTimePicker
-        value={date}
-        onChange={dateChange}
-        display={Platform.OS == 'ios'? 'inline' : 'default'}
-        themeVariant='light'
-        />
-    )}
-    </View>
-  );
+            />
+            {dateShow && (
+                <DateTimePicker
+                    value={date}
+                    onChange={dateChange}
+                    display={Platform.OS == 'ios' ? 'inline' : 'default'}
+                    themeVariant='light'
+                />
+            )}
+        </View>
+    );
 };
--- frontend/src/components/modal/ModalConfirmation.js
+++ frontend/src/components/modal/ModalConfirmation.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Pressable, Text, View } from "react-native";
-import { ButtonType, Color, ScreenSize, TextStyle } from "../../utils";
-import { ButtonHalfWidth } from "../button/ButtonHalfWidth";
-import { ButtonIcon } from "../button/ButtonIcon";
+import {Pressable, StyleSheet, Text, View} from "react-native";
+import {ButtonType, Color, customSize, ScreenSize, TextStyle} from "../../utils";
+import {ButtonHalfWidth} from "../button/ButtonHalfWidth";
+import {ButtonIcon} from "../button/ButtonIcon";
 
 export const ModalConfirmation = (props) => {
-    const { content, leftButton, rightButton } = props;
+    const {content, leftButton, rightButton, onPressOK} = props;
 
     const closeModal = (bool, data) => {
         props.changeModalVisible(bool);
@@ -12,42 +12,46 @@ export const ModalConfirmation = (props) => {
     }
 
     return (
-    <Pressable style={styles.container}>
-        <View style={styles.modal}>
-            <View style={styles.container}>
-                <ButtonIcon type={ButtonType.DEFAULT} iconName="help" customSize={1.75}/>
-            </View>
-            <Text style={{...TextStyle.h3, textAlign: 'center', fontWeight: "500",}}>{content}</Text>
-            <View style={styles.container}>
-                <View style={styles.buttonView}>
-                    <ButtonHalfWidth type={ButtonType.OUTLINE} content={leftButton} onPress={() => closeModal(false, 'leftValue')}></ButtonHalfWidth>
-                    <ButtonHalfWidth type={ButtonType.RED} content={rightButton} onPress={() => closeModal(false, 'rightValue')}></ButtonHalfWidth>
+        <Pressable style={styles.container}>
+            <View style={styles.modal}>
+                <View style={styles.container}>
+                    <ButtonIcon type={ButtonType.DEFAULT} iconName="help" customSize={1.75}/>
+                </View>
+                <Text style={{...TextStyle.h3, textAlign: 'center', marginTop: customSize(48)}}>{content}</Text>
+                <View style={styles.container}>
+                    <View style={styles.buttonView}>
+                        <ButtonHalfWidth type={ButtonType.OUTLINE} content={leftButton}
+                                         onPress={onPressOK}></ButtonHalfWidth>
+                        <ButtonHalfWidth type={ButtonType.RED} content={rightButton}
+                                         onPress={() => closeModal(false, 'rightValue')}></ButtonHalfWidth>
+                    </View>
                 </View>
             </View>
-        </View>
-    </Pressable>
+        </Pressable>
     );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
 
-  buttonView: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-evenly',
-  },
+    buttonView: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-evenly',
+    },
 
-  modal: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: Color.white_100,
-    borderRadius: 20,
-    width: ScreenSize.width,
-    height: (368 / 812) * ScreenSize.height,
-  },
+    modal: {
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: Color.white_100,
+        borderTopRightRadius: 24,
+        borderTopLeftRadius: 24,
+        width: ScreenSize.width,
+        height: (368 / 812) * ScreenSize.height,
+        paddingBottom: customSize(48)
+    },
 });
